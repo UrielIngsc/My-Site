2c382c70b5fa55785f674ee8311d7d0efd319956
Uriel Emiliano <ugalu448@gmail.com>
2022-08-11 15:36:12 -0500

    Modificacion css

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-let ocupation = ".Programador .Diseñador Web";
+function writeText(){
+    let ocupation = ".Programador .Diseñador Web";
 
-let writing = str => {
-    
-    let arrFromStr = str.split('');
-    let i = 0;
-    let printStr = setInterval(function(){
-        document.querySelector("#title-dinamic").innerHTML += arrFromStr[i];
-        i++;
-        if(i === arrFromStr.length){
-            clearInterval(printStr);
+    let writing = str => {
+        
+        let arrFromStr = str.split('');
+        let i = 0;
+        let printStr = setInterval(function(){
+            document.querySelector("#title-dinamic").innerHTML += arrFromStr[i];
+            i++;
+            if(i === arrFromStr.length){
+                clearInterval(printStr);
+            }
+        },100);   
+    }
+    writing(ocupation);
+    setTimeout(function(){
+        let icon = document.querySelector(".button-slide");
+        icon.innerHTML = "<i class='fa-solid fa-angles-down fa-3x '></i>";
+    },2800);
+
+    if($(window).width() > 800){
+        let icons = document.querySelectorAll("#custom-icon");
+        for (let i = 0; i < icons.length; i++) {
+            icons[i].classList.add("fa-2x");    
         }
-    },100);   
+    }
+    else{
+        let icons = document.querySelectorAll("#custom-icon");
+        for (let i = 0; i < icons.length; i++) {
+            icons[i].classList.add("fa-2x");  
+        }
+    }
 }
-writing(ocupation);
-setTimeout(function(){
-    let icon = document.querySelector(".button-slide");
-    icon.innerHTML = "<i class='fa-solid fa-angles-down fa-3x '></i>";
-},2800);
 
-if($(window).width() > 800){
-    let icons = document.querySelectorAll("#custom-icon");
-    for (let i = 0; i < icons.length; i++) {
-        icons[i].classList.add("fa-2x");    
-    }
-}
-else{
-    let icons = document.querySelectorAll("#custom-icon");
-    for (let i = 0; i < icons.length; i++) {
-        icons[i].classList.add("fa-2x");  
-    }
-}
+document.querySelector(".nav-toggle").addEventListener("click", ()=>{
+    document.querySelector(".nav-menu").classList.toggle("nav-menu_visible");
+})
+
+writeText();
