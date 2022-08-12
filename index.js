0c66a5bd83601786486decbf950fdefcdf49dca9
Uriel Emiliano <ugalu448@gmail.com>
2022-08-11 20:13:26 -0500

    Añadiendo estilos y cuerpo flexbox

--- index.js
+++ index.js
@@ -19,18 +19,14 @@ function writeText(){
         icon.innerHTML = "<i class='fa-solid fa-angles-down fa-3x '></i>";
     },2800);
 
-    if($(window).width() > 800){
+    if($(window).width() < 768){
         let icons = document.querySelectorAll("#custom-icon");
         for (let i = 0; i < icons.length; i++) {
+            icons[i].classList.remove("fa-3x");
             icons[i].classList.add("fa-2x");    
         }
     }
-    else{
-        let icons = document.querySelectorAll("#custom-icon");
-        for (let i = 0; i < icons.length; i++) {
-            icons[i].classList.add("fa-2x");  
-        }
-    }
+    
 }
 
 document.querySelector(".nav-toggle").addEventListener("click", ()=>{
